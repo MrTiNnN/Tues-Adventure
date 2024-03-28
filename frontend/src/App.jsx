e3fa7a5423fad40f64ed525b5c979ec81ada66ae
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-28 17:59:16 +0200

    Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
+import Hero from './home/Hero'
 import img1 from './img/1.jpg'
 import img2 from './img/2.jpg'
 import img3 from './img/3.jpg'
 // pavelpronin.com
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <img src={img1} alt="" />
-      <img src={img2} alt="" />
-      <img src={img3} alt="" />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Hero/>}/>
+        </Routes>
+
+        <img src={img1} alt="" />
+        <img src={img2} alt="" />
+        <img src={img3} alt="" />
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/home/Hero.jsx
@@ -0,0 +1,11 @@
+import './hero.css'
+
+const Hero = () => {
+    return (
+        <section className="section-hero">
+            HERO
+        </section>
+    )
+}
+
+export default Hero
