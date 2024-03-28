b10b89148b74cdc532db60ea04411b644de059b3
MrTiNnN <martin.v.velchev@gmail.com>
2024-03-28 15:52:58 +0200

    Images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import img1 from './img/1.jpg'
+import img2 from './img/2.jpg'
+import img3 from './img/3.jpg'
+// pavelpronin.com
+
 function App() {
   return (
     <>
-      
+      <img src={img1} alt="" />
+      <img src={img2} alt="" />
+      <img src={img3} alt="" />
     </>
   )
 }
