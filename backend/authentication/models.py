f6c12ad6062fd2058ec21855697cf0ae652b6321
MrTiNnN <martin.v.velchev@gmail.com>
2024-05-29 17:44:21 +0300

    Set up user model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class User(models.Model):
+    email = models.EmailField(max_length=200, null=False)
+    firstName = models.CharField(max_length=200, null=False)
+    lastName = models.CharField(max_length=200, null=False)
+    grade = models.IntegerField(null=False)
+    className = models.CharField(max_length=1, null=False)
+    password = models.CharField(max_length=200, unique=True)
+
 # Create your models here.
